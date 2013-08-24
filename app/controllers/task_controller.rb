47d6b1df251d9ca0cc42bbc531390661d80fd2ef
Taryn Sauer <taryn.sauer@gmail.com>
2013-08-23 22:24:45 -0500

    Added task viewer file, refactored task controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,24 +1,24 @@
- require_relative '../../config/application' 
+ require_relative '../../config/application'
  require 'active_record'
 
 if ARGV.any?
   if ARGV[0] == "list"
     Task.all.each do |t|
-      print "#{t.id}. #{t.description}\n"
+      listed_message(t)
     end
   elsif ARGV[0] == "add"
     task_new = Task.create(:description => ARGV[1..-1].join(' '))
-    print "Appended '#{task_new.description}' to your TODO list...\n"
+    added_message(task_new)
   elsif ARGV[0] == "delete"
-    deleted_task = Task.find(ARGV[1])
-    Task.destroy(deleted_task)
-    print "Deleted '#{deleted_task.description}' from your TODO list...\n"
+    task_to_delete = Task.find(ARGV[1])
+    Task.destroy(task_to_delete)
+    deleted_message(task_to_delete)
   elsif ARGV[0] == "complete"
-    completed_task = Task.find(ARGV[1])
-    Task.update(completed_task, :completeness => true)
-    print "Completed '#{completed_task.description}' on your TODO list...\n"
+    task_to_complete = Task.find(ARGV[1])
+    Task.update(task_to_complete, :completeness => true)
+    completed_message(task_to_complete)
   else
-    puts "That command doesn't exist!"
+    no_command_message
   end
 
 end
