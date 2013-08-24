31b15823d8fb988c0c9d8fcf6eed8ad775eb481d
Taryn Sauer <taryn.sauer@gmail.com>
2013-08-23 20:52:04 -0500

    fixed create method in controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -5,12 +5,12 @@ if ARGV.any?
   if ARGV[0] == "list"
     p Task.all
   elsif ARGV[0] == "add"
-    Task.create(ARGV[1..-1].join(' '))
+    Task.create(:description => ARGV[1..-1].join(' '))
   elsif ARGV[0] == "delete"
     Task.destroy(ARGV[1])
   elsif ARGV[0] == "complete"
     item = ARGV[1]
-    Task.update_attribute('completeness', true)
+    Task.update_attribute(:completeness, true)
   else
     puts "That command doesn't exist!"
   end
